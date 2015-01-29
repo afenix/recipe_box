e5ceba0c9d14ac4aca2e6a0277ccee78b06ce023
Gabe Finch & Alister Fenix <student@epicodus.com>
2015-01-29 15:01:53 -0800

    build basic sinatra functionality

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require('bundler/setup')
 Bundler.require(:default)
+# require('./lib/recipe')
 
-Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + './lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @recipes = Recipe.all()
   erb(:index)
+end
+
+post('/recipes') do
+  Recipe.create(:name => params.fetch('name'), :ingredients => params.fetch('ingredients'), :instructions => params.fetch('instructions'))
+  redirect('/')
 end
