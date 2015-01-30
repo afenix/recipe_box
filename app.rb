b672688a586a6f5377035ba80f1dc1aae0c6623e
Gabe Finch & Alister Fenix <student@epicodus.com>
2015-01-29 17:10:00 -0800

    Add update recipe features and rating features

--- app.rb
+++ app.rb
@@ -20,17 +20,30 @@ get('/recipes/:id') do
   erb(:recipe)
 end
 
-patch "/add_categories" do
-  category_ids = params.fetch("category_ids")
+patch ("/add_categories") do
   recipe_id = params.fetch("recipe_id").to_i()
-  tomato_soup = Recipe.find(recipe_id)
-  category_objects = []
+  found_recipe = Recipe.find(recipe_id)
+  category_ids = params.fetch("category_ids")
   category_ids.each do |id|
     this_category = Category.find(id.to_i())
-    category_objects.push(this_category)
-  end
-  category_objects.each do |object|
-    tomato_soup.categories.push(object)
+    found_recipe.categories.push(this_category)
   end
   redirect ('/')
+end
+
+get ('/edit_recipe/:id') do
+  id = params.fetch("id").to_i()
+  @recipe = Recipe.find(id)
+  erb(:edit_recipe)
+end
+
+patch('/recipes/:id') do
+  found_recipe = Recipe.find(params.fetch('id').to_i())
+  name =params.fetch('name')
+  ingredients = params.fetch('ingredients')
+  instructions = params.fetch('instructions')
+  found_recipe.update({:name => name, :ingredients => ingredients, :instructions => instructions})
+  rating = params.fetch("rating").to_i()
+  found_recipe.update({:rating => rating})
+  redirect('/')
 end
